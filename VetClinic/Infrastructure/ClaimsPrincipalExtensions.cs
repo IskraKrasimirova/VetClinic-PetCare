c4f9957a4f653b5aa1f1ccb344d9bf75228e4af6
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2022-03-20 09:28:55 +0200

    Change Layout

--- VetClinic/Infrastructure/ClaimsPrincipalExtensions.cs
+++ VetClinic/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using static VetClinic.Common.GlobalConstants;
 
 namespace VetClinic.Infrastructure
 {
@@ -7,6 +8,17 @@ namespace VetClinic.Infrastructure
         public static string GetId(this ClaimsPrincipal user)
         {
             return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+
+        public static bool IsClient(this ClaimsPrincipal user)
+            => user.IsInRole(ClientRoleName);
+
+        public static bool IsDoctor(this ClaimsPrincipal user)
+            => user.IsInRole(DoctorRoleName);
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
         }
     }
 }
