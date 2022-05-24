93b75de4dc21df64eb6d00efd7f29580e42141e8
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2022-05-24 20:34:40 +0300

    Change Login view

--- VetClinic/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VetClinic/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -59,47 +59,23 @@ namespace VetClinic.Areas.Identity.Pages.Account
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string ReturnUrl { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -121,7 +97,6 @@ namespace VetClinic.Areas.Identity.Pages.Account
                 ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
             public string PhoneNumber { get; set; }
         }
-
 
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -171,8 +146,7 @@ namespace VetClinic.Areas.Identity.Pages.Account
 
                     foreach (IdentityError error in result.Errors)
                     {
-                        this.ModelState.AddModelError(string.Empty,
-                            error.Description);
+                        this.ModelState.AddModelError(string.Empty, error.Description);
                     }
                 }
                 else
