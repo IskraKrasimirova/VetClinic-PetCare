f534fb41e6fc9b6808883f71181ab61811ba4ea2
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2022-06-08 17:41:41 +0300

    Add DepartmentService tests

---
+++ VetClinic.Test/Mocks/ClaimsPrincipalMock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace VetClinic.Test.Mocks
+{
+    public class ClaimsPrincipalMock
+    {
+        public static ClaimsPrincipal Instance(string userId = "TestUserId")
+        {
+            var fakeClaims = new List<Claim>()
+            {
+               new Claim(ClaimTypes.NameIdentifier, userId),
+            };
+
+            var fakeIdentity = new ClaimsIdentity(fakeClaims, "TestAuthType");
+
+            var mockClaimsPrincipal = new ClaimsPrincipal(fakeIdentity);
+
+            return mockClaimsPrincipal;
+        }
+    }
+}
---
+++ VetClinic.Test/ServicesTests/DepartmentServiceTests.cs
@@ -0,0 +1,340 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VetClinic.Core.Models.Departments;
+using VetClinic.Core.Services;
+using VetClinic.Data;
+using VetClinic.Data.Models;
+using VetClinic.Test.Mocks;
+using DoctorService = VetClinic.Core.Services.DoctorService;
+
+namespace VetClinic.Test.ServicesTests
+{
+    public class DepartmentServiceTests
+    {
+        //private ServiceProvider serviceProvider;
+        //private InMemoryDbContext dbContext;
+
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    dbContext = new InMemoryDbContext();
+        //    var serviceCollection = new ServiceCollection();
+
+        //    serviceProvider = serviceCollection
+        //        .AddSingleton(sp => dbContext.CreateContext())
+        //        .AddSingleton<IDepartmentService, DepartmentService>()
+        //        //.AddSingleton<IDoctorService, DoctorService>()
+        //        //.AddSingleton<IServiceService, ServiceService>()
+        //        .BuildServiceProvider();
+
+        //    var data = serviceProvider.GetRequiredService<VetClinicDbContext>();
+        //    //var userManagerMock = UserManagerMock.Instance;
+        //    //var signInManagerMock = SignInManagerMock.Instance;
+
+        //    var testDepartment = new Department
+        //    {
+        //        Id = 1,
+        //        Name = "TestDepartmentName",
+        //        Image = "TestDepartmentImg.png",
+        //        Services = new List<Service>
+        //        {
+        //            new Service
+        //            {
+        //                Id = 1,
+        //                Name = "TestService"
+        //            },
+        //            new Service
+        //            {
+        //                Id = 2,
+        //                Name = "TestService2"
+        //            }
+        //        }
+        //    };
+
+        //    data.Departments.Add(testDepartment);
+
+        //    var testDepartment2 = new Department
+        //    {
+        //        Id = 2,
+        //        Name = "TestDepartmentName2",
+        //        Image = "TestDepartmentImg2.png",
+        //        Services = new List<Service>
+        //        {
+        //            new Service
+        //            {
+        //                Id = 3,
+        //                Name = "TestService3"
+        //            },
+        //            new Service
+        //            {
+        //                Id = 4,
+        //                Name = "TestService4"
+        //            }
+        //        }
+        //    };
+
+        //    data.Departments.Add(testDepartment2);
+
+        //    data.SaveChanges();
+        //}
+
+        //[Test]
+        //public void CreateShouldWorkCorrectAndReturnDepartmentId()
+        //{
+        //    var service = serviceProvider.GetService<IDepartmentService>();
+        //    //var doctorService = serviceProvider.GetService<DoctorService>();
+        //    //var serviceService = serviceProvider.GetService<ServiceService>();
+        //    //var userManagerMock = UserManagerMock.Instance;
+        //    //var signInManagerMock = SignInManagerMock.Instance;
+        //    var result = service.Create("DepartmentName", "departmentImg.png", "description");
+        //    Assert.That(result.GetType, Is.EqualTo(typeof(int)));
+        //    Assert.That(result == 3);
+        //}
+
+
+
+        private VetClinicDbContext dbContext;
+        private DepartmentService service;
+        private DoctorService doctorService;
+        private ServiceService serviceService;
+
+        [SetUp]
+        public void Setup()
+        {
+            dbContext = DatabaseMock.Instance;
+            doctorService = new DoctorService(UserManagerMock.Instance, dbContext);
+            serviceService = new ServiceService(dbContext);
+            service = new DepartmentService(dbContext, doctorService, serviceService);
+        }
+
+        //[TearDown]
+        //public void Dispose()
+        //{
+        //    dbContext.Dispose();
+        //}
+
+        [Test]
+        public void CreateShouldWorkCorrectAndReturnDepartmentId()
+        {
+            GetDbContextWithDepartments();
+            var result = service.Create("DepartmentName", "departmentImg.png", "description");
+            Assert.That(result.GetType, Is.EqualTo(typeof(int)));
+            Assert.That(result == 3);
+            Assert.That(dbContext.Departments.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void DepartmentExistsShouldReturnTrueIfDepartmentNameExists()
+        {
+            GetDbContextWithDepartments();
+            var result = service.DepartmentExists("TestDepartmentName");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void DepartmentExistsShouldReturnFalseIfDepartmentNameNotExist()
+        {
+            GetDbContextWithDepartments();
+            var result = service.DepartmentExists("NotExistingDepartmentName");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DepartmentExistsShouldReturnTrueIfDepartmentExists()
+        {
+            GetDbContextWithDepartments();
+            var result = service.DepartmentExists(1);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void DepartmentExistsShouldReturnFalseIfDepartmentNotExist()
+        {
+            GetDbContextWithDepartments();
+            var result = service.DepartmentExists(10);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void AllDepartmentsShouldReturnAllDepartmentsNames()
+        {
+            GetDbContextWithDepartments();
+            var result = service.AllDepartments();
+            Assert.That(result.GetType, Is.EqualTo(typeof(List<string>)));
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result, Has.Member("TestDepartmentName"));
+        }
+
+        [Test]
+        public void AllDepartmentsShouldReturnEmptyCollectionWhenNoDepartmentsExist()
+        {
+            var result = service.AllDepartments();
+            Assert.That(result.Count(), Is.EqualTo(0));
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void GetAllDepartmentsShouldReturnAllDepartmentsInCorrectType()
+        {
+            GetDbContextWithDepartments();
+            var result = service.GetAllDepartments();
+            Assert.That(result.GetType, Is.EqualTo(typeof(List<DepartmentListingViewModel>)));
+            Assert.That(result.Count(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetAllDepartmentsShouldReturnNullWhenNoDepartmentsExist()
+        {
+            var result = service.GetAllDepartments();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void DetailsShouldReturnDepartmentInCorrectType()
+        {
+            GetDbContextWithDepartments();
+            var result = service.Details(1);
+            Assert.That(result.GetType, Is.EqualTo(typeof(DepartmentDetailsServiceModel)));
+        }
+
+        [Test]
+        public void DetailsShouldReturnNullWhenDepartmentNotExist()
+        {
+            GetDbContextWithDepartments();
+            var result = service.Details(10);
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void EditShouldReturnFalseWhenDepartmentNotExist()
+        {
+            GetDbContextWithDepartments();
+            var result = service.Edit(10, "testName", null, null);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EditShouldReturnTrueWhenDepartmentExists()
+        {
+            GetDbContextWithDepartments();
+            var result = service.Edit(1, "NewTestDepartmentName", "TestDepartmentImage.png", "description");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void DeleteShouldReturnFalseWhenDepartmentNotExist()
+        {
+            GetDbContextWithDepartmentsAndDoctors();
+            var result = service.Delete(10);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DeleteShouldReturnTrueWhenDepartmentExists()
+        {
+            GetDbContextWithDepartmentsAndDoctors();
+            var result = service.Delete(1);
+            Assert.That(result, Is.True);
+        }
+
+        private void GetDbContextWithDepartments()
+        {
+            var testDepartment = new Department
+            {
+                Id = 1,
+                Name = "TestDepartmentName",
+                Image = "TestDepartmentImg.png",
+                Services = new List<Service>
+                {
+                    new Service
+                    {
+                        Id = 1,
+                        Name = "TestService"
+                    },
+                    new Service
+                    {
+                        Id = 2,
+                        Name = "TestService2"
+                    }
+                }
+            };
+
+            dbContext.Departments.Add(testDepartment);
+
+            var testDepartment2 = new Department
+            {
+                Id = 2,
+                Name = "TestDepartmentName2",
+                Image = "TestDepartmentImg2.png",
+                Services = new List<Service>
+                {
+                    new Service
+                    {
+                        Id = 3,
+                        Name = "TestService3"
+                    },
+                    new Service
+                    {
+                        Id = 4,
+                        Name = "TestService4"
+                    }
+                }
+            };
+
+            dbContext.Departments.Add(testDepartment2);
+
+            dbContext.SaveChanges();
+        }
+        private void GetDbContextWithDepartmentsAndDoctors()
+        {
+            var department = new Department
+            {
+                Id = 1,
+                Name = "TestDepartment",
+                Services = new List<Service>
+                {
+                    new Service
+                    {
+                        Id = 1,
+                        Name = "TestService"
+                    },
+                    new Service
+                    {
+                        Id = 2,
+                        Name = "TestService2"
+                    }
+                }
+            };
+
+            dbContext.Departments.Add(department);
+
+            var userDoctor = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = "testDoctor@vetclinic.com",
+                UserName = "testDoctor@vetclinic.com",
+                FullName = "TestDoctorFullName"
+            };
+
+            dbContext.Users.Add(userDoctor);
+
+            var doctor = new Doctor
+            {
+                Id = "testDoctorId",
+                UserId = userDoctor.Id,
+                Email = "testDoctor@vetclinic.com",
+                FullName = "TestDoctorFullName",
+                PhoneNumber = "0111222333",
+                ProfileImage = "https://res.cloudinary.com/dqnorpdaj/image/upload/v1648707675/VetClinic/testDoctor.png",
+                DepartmentId = 1
+            };
+
+            dbContext.Doctors.Add(doctor);
+            department.Doctors.Add(doctor);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
